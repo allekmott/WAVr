eb42d650c87e138c5b1c9e2051aa2d080d1221d2
tchieze <allekmott@gmail.com>
2015-12-20 12:43:15 -0700

    Initialize header values

--- wavr.c
+++ wavr.c
@@ -6,10 +6,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 #include "wav.h"
 
-#define WAVR_VERSION "0.0.5"
+#define WAVR_VERSION "0.0.7"
+#define WAV_SAMPLE_RATE 44100
+#define WAV_LENGTH 2
 
 void usage(const char *cmd) {
 	printf("Usage: %s <args>\n", cmd);
@@ -30,15 +33,39 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
+	int fullHeaderSize = sizeof(struct WavHeader)
+		+ sizeof(struct FormatHeader)
+		+ sizeof(struct DataHeader);
+
 	struct WavHeader *wavHeader;
 	struct FormatHeader *formatHeader;
 	struct DataHeader *dataHeader;
 
-	void *headerSpace = malloc(sizeof(struct WavHeader)
-		+ sizeof(struct FormatHeader)
-		+ sizeof(struct DataHeader));
+	void *headerSpace = malloc(fullHeaderSize);
 	wavHeader = (struct WavHeader *)headerSpace;
 	formatHeader = (struct FormatHeader *)(wavHeader + 1);
 	dataHeader = (struct DataHeader *)(formatHeader + 1);
 
+	/* begin populating headers with values */
+
+	/* wav-specific stuff */
+	strcpy(wavHeader->ChunkID, "RIFF");
+	wavHeader->ChunkSize = (uint32_t) fullHeaderSize - 8;
+	strcpy(wavHeader->RIFFType, "WAVE");
+
+	/* format-specific stuff */
+	strcpy(formatHeader->ChunkID, "fmt ");
+	formatHeader->ChunkSize = 16;
+	formatHeader->CompressionCode = 1;
+	formatHeader->Channels = 1;
+	formatHeader->SampleRate = (uint32_t) WAV_SAMPLE_RATE; /* lol */
+	formatHeader->SigBitsPerSamp = 16;
+	formatHeader->BlockAlign = 2;
+	formatHeader->AvgBytesPerSec = formatHeader->BlockAlign * WAV_SAMPLE_RATE;
+
+	/* data-specific stuff */
+	strcpy(dataHeader->ChunkID, "data");
+	dataHeader->ChunkSize = WAV_LENGTH * WAV_SAMPLE_RATE; /* lol */
+
+	free(headerSpace);
 }
