dbcb750364cd2be654b43bd34295365627a9a046
tchieze <allekmott@gmail.com>
2015-12-20 12:14:36 -0700

    Reorganize wav structs into separate header

---
+++ wav.h
@@ -0,0 +1,31 @@
+/* wav.h
+ * Wav structs & shit
+ * Created: 20 Dec 2014
+ */
+
+#ifndef _WAV_H
+#define _WAV_H
+
+struct WavHeader {
+	char ChunkID[4];
+	uint32_t ChunkSize;
+	char RIFFType[4];
+};
+
+struct FormatHeader {
+	char ChunkID[4];
+	uint32_t ChunkSize;
+	uint16_t CompressionCode;
+	uint16_t Channels;
+	uint32_t SampleRate;
+	uint32_t AvgBytesPerSec;
+	uint16_t BlockAlign;
+	uint16_t SigBitsPerSamp;
+};
+
+struct DataHeader {
+	char ChunkId[4];
+	uint32_t ChunkSize;
+};
+
+#endif
---
+++ wavr.c
@@ -0,0 +1,37 @@
+/* wavr.c
+ * Main thingy
+ * 5p4wn wav file
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <math.h>
+#include <unistd.h>
+
+#include "wav.h"
+
+#define WAVR_VERSION "0.0.2"
+
+void usage(const char *cmd) {
+	printf("Usage: %s <args>\n", cmd);
+	exit(0);
+}
+
+int main(int argc, char *argv[]) {
+	printf("WavR v%s\n", WAVR_VERSION);
+
+	int c;
+	char *out_file;
+	extern int optind, optopt;
+
+	while ((c = getopt(argc, argv, "o:")) != -1) {
+		switch (c) {
+			case 'o': out_file = optarg; break;
+			case '?': usage(argv[0]); break;
+		}
+	}
+
+}
